c65bae88e8aa957e8166c5ebea8b601a4ea9763a
Serhii Kredentser <sergeykredenser@gmail.com>
2021-11-07 18:30:21 +0200

    change llink to style

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyle() {
-    return gulp.src('./style/**/*.scss').pipe(sass()).pipe(gulp.dest('./style/'));
+    return gulp.src('./style/**/*.scss').pipe(sass()).pipe(gulp.dest('./'));
 }
 
 gulp.task('watch', function() {
